193c4e64b114a1350279c45964ba4220afaffe9d
Simon Jacobsen <simon.jac@hotmail.com>
2020-04-16 09:09:33 +0200

    Tried to add "Damage Objects"

---
+++ Assets/RedFlashScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedFlashScript : MonoBehaviour
+{
+    public bool isDamaged = false;
+    [SerializeField]
+    private float counter;
+    public float flashTime = 1;
+
+    private void Start()
+    {
+        isDamaged = false;
+        gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        if (isDamaged)
+        {
+            counter += flashTime * Time.deltaTime;
+        }
+
+        if(counter > 1)
+        {
+            isDamaged = false;
+            counter = 0;
+        }
+
+        if (counter > 0.01f)
+        {
+            gameObject.SetActive(true);
+
+        } else
+        {
+            //gameObject.SetActive(false);
+        }
+
+    }
+}
