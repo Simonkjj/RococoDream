33cf2eb3295e4d483c4d783f8849125bb908b031
Lukas <Tsuki32@hotmail.com>
2020-04-26 13:54:25 +0200

    Added Ending Credits

---
+++ Assets/Scripts/CreditsAnimation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsAnimation : MonoBehaviour
+{
+    public void ReturnMainmenu()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+}
