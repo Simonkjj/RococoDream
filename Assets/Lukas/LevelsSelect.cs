c85f8efc152da8d3d31bae7703ca40df7787091b
Lukas <Tsuki32@hotmail.com>
2020-04-27 15:18:39 +0200

    Added LevelSelection to mainmenu

---
+++ Assets/Lukas/LevelsSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelsSelect : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level 1 Prototype");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+}
